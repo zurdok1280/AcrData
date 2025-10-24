67c1c5125864e056df1e25188d5649adb0494dba
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2025-10-24 00:58:00 -0600

    Update SecurityConfig.java

--- AcrData/src/main/java/com/ml/config/SecurityConfig.java
+++ AcrData/src/main/java/com/ml/config/SecurityConfig.java
@@ -49,7 +49,6 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
         configuration.setAllowedOrigins(Arrays.asList("https://digital-latino.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
